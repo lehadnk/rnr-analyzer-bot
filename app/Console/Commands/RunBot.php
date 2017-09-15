af46f0b37d8f9b29608a4212c3e89fcebd4dc473
Alexey Zauzin <lehadnk@gmail.com>
2017-09-15 05:10:50 +0300

    var_dump errors for bot

--- app/Console/Commands/RunBot.php
+++ app/Console/Commands/RunBot.php
@@ -15,6 +15,7 @@ use BotMan\BotMan\Cache\ArrayCache;
 use BotMan\BotMan\Drivers\DriverManager;
 use BotMan\Drivers\Discord\DiscordDriver;
 use Illuminate\Console\Command;
+use Mockery\Exception;
 use React\EventLoop\Factory;
 use BotMan\BotMan\Middleware\ApiAi;
 
@@ -51,71 +52,76 @@ class RunBot extends Command
      */
     public function handle()
     {
-        $apiAi = \App\Helpers\ApiAi::create('22b84338101e4215bf593bb1353af81c')->listenForAction();
+        try {
+            $apiAi = \App\Helpers\ApiAi::create('22b84338101e4215bf593bb1353af81c')->listenForAction();
 
-        $loop = Factory::create();
-        DriverManager::loadDriver(DiscordDriver::class);
-        $botman = BotManFactory::createForDiscord(config('botman', [
-            'discord' => [
-                'token' => env('DISCORD_BOT_TOKEN'),
-            ]
-        ]), $loop, new ArrayCache());
+            $loop = Factory::create();
+            DriverManager::loadDriver(DiscordDriver::class);
+            $botman = BotManFactory::createForDiscord(config('botman', [
+                'discord' => [
+                    'token' => env('DISCORD_BOT_TOKEN'),
+                ]
+            ]), $loop, new ArrayCache());
 
-        $botman->middleware->received($apiAi);
+            $botman->middleware->received($apiAi);
 
-        $botman->hears('', function(BotMan $bot) {
-            $botUserId = env('DISCORD_BOT_USER_ID');
-            if ($bot->getMessage()->getSender() == $botUserId) {
-                return;
-            }
-            if (!stristr($bot->getMessage()->getText(), "<@$botUserId>")) {
-                return;
-            }
-
-            $extras = $bot->getMessage()->getExtras();
-            $apiReply = $extras['apiReply'] ?? null;
-            $apiAction = $extras['apiAction'] ?? null;
-            $apiIntent = $extras['apiIntent'] ?? null;
-            $apiParameters = $extras['apiParameters'] ?? [];
-
-            if ($apiReply) {
-                $bot->reply($apiReply);
-            } else {
-                $this->info($apiIntent);
-                $this->info(print_r($apiParameters, true));
-
-                if ($apiIntent == 'best-attempt') {
-                    $conversation = new BestAttempt();
+            $botman->hears('', function (BotMan $bot) {
+                var_dump($bot->getMessage()->getText());
+                $botUserId = env('DISCORD_BOT_USER_ID');
+                if ($bot->getMessage()->getSender() == $botUserId) {
+                    return;
                 }
-                if ($apiIntent == 'math') {
-                    $conversation = new Math();
-                }
-                if ($apiIntent == 'last-log') {
-                    $conversation = new LastLog();
-                }
-                if ($apiIntent == 'how-was-your-raid') {
-                    $conversation = new HowWasYourRaid();
-                }
-                if ($apiIntent == 'fails-on-ability') {
-                    $conversation = new FailsOnAbility();
-                }
-                if ($apiIntent == 'player-fail-detail') {
-                    $conversation = new PlayerFailDetail();
-                }
-                if ($apiIntent == 'update-log') {
-                    $conversation = new UpdateLog();
+                if (!stristr($bot->getMessage()->getText(), "<@$botUserId>")) {
+                    return;
                 }
 
-                $conversation->setApiParameters($apiParameters);
-                try {
-                    $bot->startConversation($conversation);
-                } catch (\Exception $e) {
-                    $bot->reply("Хуйню какую-то понаписали: {$e->getMessage()}");
+                $extras = $bot->getMessage()->getExtras();
+                $apiReply = $extras['apiReply'] ?? null;
+                $apiAction = $extras['apiAction'] ?? null;
+                $apiIntent = $extras['apiIntent'] ?? null;
+                $apiParameters = $extras['apiParameters'] ?? [];
+
+                if ($apiReply) {
+                    $bot->reply($apiReply);
+                } else {
+                    $this->info($apiIntent);
+                    $this->info(print_r($apiParameters, true));
+
+                    if ($apiIntent == 'best-attempt') {
+                        $conversation = new BestAttempt();
+                    }
+                    if ($apiIntent == 'math') {
+                        $conversation = new Math();
+                    }
+                    if ($apiIntent == 'last-log') {
+                        $conversation = new LastLog();
+                    }
+                    if ($apiIntent == 'how-was-your-raid') {
+                        $conversation = new HowWasYourRaid();
+                    }
+                    if ($apiIntent == 'fails-on-ability') {
+                        $conversation = new FailsOnAbility();
+                    }
+                    if ($apiIntent == 'player-fail-detail') {
+                        $conversation = new PlayerFailDetail();
+                    }
+                    if ($apiIntent == 'update-log') {
+                        $conversation = new UpdateLog();
+                    }
+
+                    $conversation->setApiParameters($apiParameters);
+                    try {
+                        $bot->startConversation($conversation);
+                    } catch (\Exception $e) {
+                        $bot->reply("Хуйню какую-то понаписали: {$e->getMessage()}");
+                    }
+
                 }
+            });
 
-            }
-        });
-
-        $loop->run();
+            $loop->run();
+        } catch (Exception $e) {
+            var_dump($e->getMessage());
+        }
     }
 }
